2bbe2570e6d75bc9f12de3c1426150e617fa7ec9
AmoghJM <amoghmoger@gmail.com>
2025-05-07 09:47:13 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('button');
+const jokes = document.querySelector('#jokes');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLI = document.createElement('li');
+    newLI.append(jokeText);
+    jokes.append(newLI);
+}
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: 'application/json' } };
+        const res = await axios.get('https://icanhazdadjoke.com/', config);
+        return res.data.joke;
+    } catch (e) {
+        return "No jokes available!"
+    }
+}
+button.addEventListener('click', addNewJoke)
